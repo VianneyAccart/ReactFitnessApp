347b1a343b6013888231e5d2d8f4c54e90713927
Vianney Accart <vianneyaccart@gmail.com>
2022-08-21 23:02:06 +0200

    passage en function component

--- src/Exercise/ExerciseList.js
+++ src/Exercise/ExerciseList.js
@@ -1,4 +1,4 @@
-import { Component } from "react";
+import React, { useEffect } from "react";
 import ActiveFilter from "../Filtering/ActiveFilter";
 import Exercise from "./Exercise";
 import FilterDropdown from "../Filtering/FilterDropdown";
@@ -6,89 +6,74 @@ import "../index.css";
 import axios from "axios";
 import SearchBar from "../Filtering/SearchBar";
 
-class ExerciceList extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      exercises: [],
-      selectedGroup: "",
-      searchValue: "",
-    };
-    this.initialExercises = [];
-  }
+export default function ExerciceList() {
+  const [exercises, setExercises] = React.useState([]);
+  const [initialExercises, setInitialExercises] = React.useState(exercises);
+  const [selectedGroup, setSelectedGroup] = React.useState("");
+  const [searchValue, setSearchValue] = React.useState("");
 
-  componentDidMount() {
-    this.getExercises();
-  }
+  useEffect(() => {
+    getExercises();
+  }, []);
 
-  getExercises = () => {
+  const getExercises = () => {
+    console.log("api call");
     axios
       .get("https://mocki.io/v1/2f2ca3e8-5d39-499d-983e-266957dbbea3")
       .then((response) => response.data)
       .then((data) => {
-        this.setState({ exercises: data });
-        this.initialExercises = data;
+        setExercises(data);
+        setInitialExercises(data);
       });
   };
 
-  showExercisesFilteredByMuscularGroup = (muscularGroup) => {
-    let filteredExercises = this.state.exercises.filter(
+  const showExercisesFilteredByMuscularGroup = (muscularGroup) => {
+    let filteredExercises = exercises.filter(
       (exercise) => exercise.muscularGroup === muscularGroup
     );
-    this.setState({
-      exercises: filteredExercises,
-      selectedGroup: muscularGroup,
-    });
+    setExercises(filteredExercises);
+    setSelectedGroup(muscularGroup);
   };
 
-  displayAllExercises = () => {
-    this.setState({ exercises: this.initialExercises, selectedGroup: "" });
+  const displayAllExercises = () => {
+    setExercises(initialExercises);
+    setSelectedGroup("");
   };
 
-  searchValueChange = (value) => {
-    this.setState({ searchValue: value }, this.showState);
+  const searchValueChange = (value) => {
+    setSearchValue(value);
   };
 
-  showState = () => {
-    console.log(this.state.searchValue);
-  };
-
-  render() {
-    return (
-      <div className="container">
-        <div className="d-flex justify-content-between my-3">
-          <h1>Liste des exercices</h1>
-          <div className="d-flex">
-            <SearchBar searchValueChange={this.searchValueChange} />
-            <FilterDropdown
-              displayAllExercises={this.displayAllExercises}
-              showExercisesFilteredByMuscularGroup={
-                this.showExercisesFilteredByMuscularGroup
-              }
-            />
-          </div>
-        </div>
-
-        {this.state.selectedGroup ? (
-          <ActiveFilter
-            selectedGroup={this.state.selectedGroup}
-            displayAllExercises={this.displayAllExercises}
+  return (
+    <div className="container">
+      <div className="d-flex justify-content-between my-3">
+        <h1>Liste des exercices</h1>
+        <div className="d-flex">
+          <SearchBar searchValueChange={searchValueChange} />
+          <FilterDropdown
+            displayAllExercises={displayAllExercises}
+            showExercisesFilteredByMuscularGroup={
+              showExercisesFilteredByMuscularGroup
+            }
           />
-        ) : (
-          ""
-        )}
-        <ul className="list-group list-group-flush">
-          {this.state.exercises
-            .filter((exercise) =>
-              exercise.name.includes(this.state.searchValue)
-            )
-            .map((exercise, index) => {
-              return <Exercise key={index} exercise={exercise} />;
-            })}
-        </ul>
+        </div>
       </div>
-    );
-  }
-}
 
-export default ExerciceList;
+      {selectedGroup ? (
+        <ActiveFilter
+          selectedGroup={selectedGroup}
+          displayAllExercises={displayAllExercises}
+        />
+      ) : (
+        ""
+      )}
+      <ul className="list-group list-group-flush">
+        {exercises
+          .filter((exercise) => exercise.name.includes(searchValue))
+          .map((exercise, index) => {
+            return <Exercise key={index} exercise={exercise} />;
+          })}
+      </ul>
+    </div>
+  );
+}
